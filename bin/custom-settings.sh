c87502c3ea76b279c5ff09a50e6824823ee65245
Jayke Peters <37952082+jaykepeters@users.noreply.github.com>
2018-08-16 03:07:53 -0500

    Update custom-settings.sh

--- bin/custom-settings.sh
+++ bin/custom-settings.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Pi-hole on Google Cloud Platform Enhancement Script 
-# PGCPES
-# Last Modified by JP on 1 August, 2018
+# Last Modified by JP on 16 August, 2018
+# Set Global Variables
+myhostname=$(cat /etc/UniPi/hostname)
+
 # Force nameservers and not Google's by deleting /etc/resolv.conf
 /bin/rm -rf /etc/resolv.conf
 
 # Set the hostname
-/usr/bin/hostnamectl set-hostname dns-a.jpits.us
+/usr/bin/hostnamectl set-hostname $myhostname
