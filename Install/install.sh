b4efd5520624b4d0899bca53bcc8ec7e565fa44f
Jayke Peters <37952082+jaykepeters@users.noreply.github.com>
2018-08-26 07:06:45 -0500

    Update install.sh

--- Install/install.sh
+++ Install/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env
 # UniPi Installer Script
-# Quick Execution: curl -sSL https://myrawurl | bash
+# Quick Execution: curl -sSL https://raw.githubusercontent.com/jaykepeters/UniPi/master/Install/install.sh | bash
