8d0fa7d901ee303d5f280dfd39dd0418c36a03d1
Jayke Peters <jaykepeters@gmail.com>
2019-03-25 19:41:24 -0500

    Create airprint.sh

---
+++ usr/local/bin/airprint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+printuser="jaykepeters"
+
+# Update 
+apt-get update -y
+
+# Upgrade
+apt-get upgrade -y
+
+# CUPS
+apt-get install -y cups
+
+# Printer Admin
+usermod -a -G lpadmin $printuser
+
+# Settings
+cupsctl --remote-any
+/etc/init.d/cups restart
+
+# Done!
+echo "CUPS Successfully Installed!"
+
+# foo2zjs
+cd /tmp
+wget -O foo2zjs.tar.gz http://foo2zjs.rkkda.com/foo2zjs.tar.gz
+tar zxf foo2zjs.tar.gz
+cd foo2zjs
+make
+make install
+make cups
+
+# Done!
+echo "foo2zjs Successfully Installed!"
